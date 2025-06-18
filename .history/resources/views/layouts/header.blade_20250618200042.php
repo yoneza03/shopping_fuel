5e257ae757385e3469ced2f47672413c95358cdc
yoneza03 <sy.4195Lapin@outlook.com>
2025-06-18 22:05:40 +0900

    20250618 - ホーム画面 - 実装中

--- .history/resources/views/layouts/header.blade_20250618200042.php
+++ .history/resources/views/layouts/header.blade_20250618200042.php
@@ -1,6 +1,8 @@
 <header class="d-flex justify-content-between align-items-center p-3 bg-light">
     <a href="{{ route('login') }}">
-        <img src="{{ asset('images/logo.png') }}" alt="ロゴ" class="logo"></a>
+        <img src="{{ asset('images/logo.png') }}" alt="ロゴ" class="logo">
+        {{-- <img src="{{ asset('public/images/logo.png') }}" alt="ロゴ" class="logo"></a> --}}
+        {{-- <img src="/images/logo.png" alt="ロゴ" class="logo"> --}}
 
     @if(Auth::check()) {{-- ログイン済みなら表示 --}}
         @if(request()->routeIs('home')) {{-- ホーム画面なら表示 --}}
--- .history/resources/views/layouts/header.blade_20250618200159.php
+++ .history/resources/views/layouts/header.blade_20250618200159.php
@@ -1,6 +1,6 @@
 <header class="d-flex justify-content-between align-items-center p-3 bg-light">
     <a href="{{ route('login') }}">
-        <img src="{{ asset('images/logo.png') }}" alt="ロゴ" class="logo"></a>
+        <img src="{{ asset('images/logo.png') }}" alt="ロゴ" class="logo">
 
     @if(Auth::check()) {{-- ログイン済みなら表示 --}}
         @if(request()->routeIs('home')) {{-- ホーム画面なら表示 --}}
